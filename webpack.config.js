683be06912aed78cdd909e975bd6e66c8d9cc4a9
Luke <lartnak@gmail.com>
2016-12-07 16:41:32 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./app/index.js', './css/master.css'],
+  output: {
+    path: './build',
+    filename: 'app.js'
+  },
+  resolve: {
+    root: path.resolve(__dirname, 'app'),
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
+  postcss: function() {
+    return [require('precss'), require('autoprefixer'), require('cssnano')]
+  }
+}
