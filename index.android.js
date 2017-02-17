ba623502452f177c6864a97f9830b92d6454874c
Luke <lartnak@gmail.com>
2017-02-17 16:27:02 -0500

    setup toolbar and nav drawer

--- index.android.js
+++ index.android.js
@@ -8,46 +8,87 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  StatusBar,
+  DrawerLayoutAndroid,
+  ScrollView,
+  ToolbarAndroid,
   Text,
+  ListView,
   View
 } from 'react-native';
 
-export default class lurker extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+import hamburger from './icons/ic_menu_white_24dp.png';
+
+export default class Lurker extends Component {
+
+  constructor() {
+
+    super();
+
+    const ds = new ListView.DataSource({rowHasChanged: (a, b) => a !== b});
+
+    this.state = {posts: ds.cloneWithRows(['Hello World'])};
+
   }
+
+  render() {
+
+    let navigationView = (
+      <ScrollView>
+        <View style = {{height:100, backgroundColor: '#333', justifyContent: 'center'}}>
+           <Text style = {{height:25, color:'white', paddingLeft: 25}}>Welcome To ReactNative</Text>
+        </View>
+      </ScrollView>
+    )
+
+    return (
+
+      <DrawerLayoutAndroid
+
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}
+        ref="drawer">
+
+        <StatusBar
+          backgroundColor="#1a1a1a"
+          barStyle="light-content"/>
+
+        <ToolbarAndroid
+          style={styles.toolbar}
+          title="Lurker"
+          titleColor="#ffffff"
+          navIcon={hamburger}
+          onIconClicked={() => this.refs['drawer'].openDrawer()}
+        />
+
+        <ScrollView>
+
+          <ListView
+            dataSource={this.state.posts}
+            renderRow={post => <Text>{post}</Text>} />
+
+        </ScrollView>
+
+
+
+      </DrawerLayoutAndroid>
+
+    )
+
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  toolbar: {
+    backgroundColor: '#2a2a2a',
+    height: 56
+  }
 });
 
-AppRegistry.registerComponent('lurker', () => lurker);
+AppRegistry.registerComponent('lurker', () => Lurker);
